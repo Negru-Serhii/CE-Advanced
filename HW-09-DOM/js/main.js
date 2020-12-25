aa325203f45ca544ca9cf10ea25deddc3f6629c7
Negru Sergey <negrusergey@ukr.net>
2020-12-25 21:28:15 +0200

    js fill

---
+++ HW-09-DOM/js/main.js
@@ -0,0 +1,48 @@
+const getRadomColor = () => {
+  const newColor =
+    "rgb" +
+    "(" +
+    Math.floor(Math.random() * 256) +
+    "," +
+    Math.floor(Math.random() * 256) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    ")";
+  return newColor;
+};
+const createPlace = () => {
+  const place = document.createElement(`div`);
+  place.classList.add("container");
+  place.style.width = "250px";
+  place.style.height = "250px";
+  place.style.display = "flex";
+  place.style.flexWrap = "wrap";
+  document.body.append(place);
+  return place;
+};
+const createBlock = () => {
+  const block = document.createElement(`div`);
+  block.style.width = "50px";
+  block.style.height = "50px";
+  block.style.backgroundColor = getRadomColor();
+  return block;
+};
+const generateBlocks = (element) => {
+  for (let i = 0; i < 25; i++) {
+    element.append(createBlock());
+  }
+};
+const place = createPlace();
+generateBlocks(place);
+
+//Advanced
+
+const generateBlocksInterval = () => {
+  setInterval(function () {
+    const blocks = place.children;
+    for (let i = 0, block; (block = blocks[i]); i++) {
+      block.style.backgroundColor = getRadomColor();
+    }
+  }, 1000);
+};
+generateBlocksInterval();
