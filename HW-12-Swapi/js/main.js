f53275467d11f61c3cf295842f42862b71336139
Negru Sergey <negrusergey@ukr.net>
2021-01-20 18:37:33 +0200

    fill all files and folders

--- HW-12-Swapi/js/main.js
+++ HW-12-Swapi/js/main.js
@@ -2,25 +2,27 @@ const selectEpisode = document.getElementById(`select-episode`);
 const showInfo = document.getElementById(`show-info`);
 const charactersContainer = document.querySelector(".characters-container");
 const getPlanets = document.getElementById("getPlanets");
+const episode = selectEpisode.value;
 
 const getCharacters = (episode) => {
   axios
-    .get("https://swapi.dev/api/people/")
-    .then((res) =>
-      res.data.results
-        .filter((character) =>
-          character.films.includes(`http://swapi.dev/api/films/${episode}/`)
-        )
-        .map(
-          (character) =>
-            (charactersContainer.innerHTML += `Ім'я:${character?.name}, Дата народження: ${character?.birth_year}, Стать: ${character?.gender}</br>`)
-        )
-    );
+    .get(`http://swapi.dev/api/films/${selectEpisode.value}/`)
+    .then((res) => {
+      const characters = res.data.characters;
+      characters.map((url) => {
+        axios.get(url).then((results) => {
+          const character = results.data;
+          charactersContainer.innerHTML += `Ім'я:${character?.name}, Дата народження: ${character?.birth_year}, Стать: ${character?.gender}</br>`;
+        });
+      });
+    });
 };
+selectEpisode.addEventListener("click", () => {
+  charactersContainer.innerHTML = "";
+});
 
 showInfo.addEventListener("click", () => {
-  const episode = selectEpisode.value;
-
+  charactersContainer.innerHTML = "";
   getCharacters(episode);
 });
 
--- HW-12-Swapi/js/planets.js
+++ HW-12-Swapi/js/planets.js
@@ -1,8 +1,9 @@
 const listPlanets = document.getElementById("listPlanets");
-fetch(`https://swapi.dev/api/planets/`)
-  .then((response) => response.json())
-  .then((data) =>
-    data.results.map(
-      (planet) => (listPlanets.innerHTML += `${planet.name}</br>`)
+axios
+  .get(`https://swapi.dev/api/planets/`)
+  .then((res) =>
+    res.data.results.map(
+      (planet) =>
+        (listPlanets.innerHTML += `Planet: ${planet.name}; Diameter: ${planet.diameter}; Gravity: ${planet.gravity};</br> Population: ${planet.population}.</br>`)
     )
   );
